68cde434ba00a4a40068a3f032ab3e031f036976
firedenx12 <firman.tkj5@gmail.com>
2023-06-26 22:56:16 +0800

    update absensi

--- app/src/main/java/com/example/absendisbun/service/ApiInterface.java
+++ app/src/main/java/com/example/absendisbun/service/ApiInterface.java
@@ -2,14 +2,19 @@ package com.example.absendisbun.service;
 
 import com.example.absendisbun.service.response.listabsensi.ResponseListAbsensi;
 import com.example.absendisbun.service.response.login.ResponseLogin;
+import com.example.absendisbun.service.response.postabsensi.ResponsePostAbsensi;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
@@ -25,6 +30,16 @@ public interface ApiInterface {
     })
     @GET("absensi")
     Call<ResponseListAbsensi> getListAbsensi(@Header("Authorization") String token);
+
+    @Multipart
+    @Headers({
+            "Accept: application/json",
+    })
+    @POST("postAbsensi")
+    Call<ResponsePostAbsensi> postAbsensi (@Header("Authorization") String token,
+                                               @Part("latitude") RequestBody latitude,
+                                               @Part("longitude") RequestBody longitude,
+                                               @Part MultipartBody.Part media);
 //
 //    @FormUrlEncoded
 //    @POST("hasil_votings")
